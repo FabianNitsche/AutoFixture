f3f0267455edf6e77e10504359b2f9a39b3f42ff
FabianNitsche <ifab@freenet.de>
2021-07-01 20:57:31 +0200

    Concept study for ValueObject EqualsAssertion.

---
+++ Src/Idioms/ReflectionScaffolding.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFixture.Idioms
+{
+    public class ReflectionScaffolding
+    {
+        private readonly List<FieldAccess> allFields = new List<FieldAccess>();
+
+        private readonly Type type;
+
+        public FieldAccess<int>[] IntegerFields => allFields.OfType<FieldAccess<int>>().ToArray();
+
+        public FieldAccess<string>[] StringFields => allFields.OfType<FieldAccess<string>>().ToArray();
+
+        public ReflectionScaffolding(Type type, object instance) : this(type, instance, new HashSet<object>(EqualityComparer<object>.Default)) { }
+
+        public ReflectionScaffolding(Type type, object instance, HashSet<object> cycleBreaker)
+        {
+            this.type = type;
+
+            var fields = type.GetFields(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.FlattenHierarchy);
+            foreach (var field in fields)
+            {
+                if (field.FieldType == typeof(int))
+                {
+                    AddPrimitive<int>(field, instance);
+                }
+                else if (field.FieldType == typeof(string))
+                {
+                    AddPrimitive<string>(field, instance);
+                }
+                else if (field.FieldType.IsPrimitive is false)
+                {
+                    AddNonPrimitiveFields(field, instance, cycleBreaker);              
+                }
+            }
+        }
+
+        private void AddPrimitive<T>(FieldInfo fieldInfo, object instance)
+        {
+            allFields.Add(new FieldAccess<T>(fieldInfo, instance));
+        }
+
+        private void AddNonPrimitiveFields(FieldInfo fieldInfo, object instance, HashSet<object> cycleBreaker)
+        {
+            var nestedInstance = fieldInfo.GetValue(instance);
+
+            if (cycleBreaker.Contains(nestedInstance) || nestedInstance == null)
+            {
+                return;
+            }
+            cycleBreaker.Add(nestedInstance);
+
+            allFields.AddRange(new ReflectionScaffolding(fieldInfo.FieldType, nestedInstance).allFields);
+        }
+
+        public ReflectionScaffolding Clone()
+        {
+            var clone = FormatterServices.GetUninitializedObject(type);
+            var cloneScaffolding = new ReflectionScaffolding(type, clone);
+            for (int i = 0; i < cloneScaffolding.allFields.Count; i++)
+            {
+                var cloneField = cloneScaffolding.allFields[i];
+                var originalField = allFields[i];
+                cloneField.SetObject(originalField.GetObject());
+            }
+            return cloneScaffolding;
+        }
+
+        public class FieldAccess<T> : FieldAccess
+        {
+            public FieldAccess(FieldInfo fieldInfo, object instance) : base(fieldInfo, instance)
+            {
+            }
+
+            public T Get() => (T)GetObject();
+
+            public void Set(T value) => SetObject(value);
+        }
+
+        public class FieldAccess
+        {
+            private readonly FieldInfo fieldInfo;
+
+            private readonly object instance;
+
+            public FieldAccess(FieldInfo fieldInfo, object instance)
+            {
+                this.fieldInfo = fieldInfo;
+                this.instance = instance;
+            }
+
+            public object GetObject() => fieldInfo.GetValue(instance);
+
+            public void SetObject(object value)
+            {
+                fieldInfo.SetValue(instance, value);
+            }
+        }
+
+    }
+}
---
+++ Src/IdiomsUnitTest/ReflectionScaffoldingTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture.Idioms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AutoFixture.IdiomsUnitTest
+{
+    public class ReflectionScaffoldingTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var testInstance = new TestClass { NestedInstance = new TestClass() };
+            var sut = new ReflectionScaffolding(typeof(TestClass), testInstance);
+
+            sut.IntegerFields[0].Set(1);
+            sut.IntegerFields[1].Set(2);
+            sut.IntegerFields[2].Set(3);
+            sut.IntegerFields[3].Set(4);
+            sut.StringFields[0].Set("Test");
+            sut.StringFields[1].Set("Test2");
+
+            Assert.Equal(1, testInstance.IntTest);
+            Assert.Equal(2, testInstance.IntAutoProperty);
+            Assert.Equal("Test", testInstance.StringTest);
+            Assert.Equal(3, testInstance.NestedInstance.IntTest);
+            Assert.Equal(4, testInstance.NestedInstance.IntAutoProperty);
+            Assert.Equal("Test2", testInstance.NestedInstance.StringTest);
+        }
+
+        public class TestClass
+        {
+            private readonly int intTest;
+
+            private string stringTest;
+
+            public int IntTest => intTest;
+
+            public string StringTest => stringTest;
+
+            public int IntAutoProperty { get; }
+
+            public TestClass NestedInstance { get; set; }
+        }
+    }
+}
